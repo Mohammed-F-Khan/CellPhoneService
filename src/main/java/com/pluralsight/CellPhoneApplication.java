d7686d49d7f008f434f16216bf7dda0b936f2be7
MohammedKhan <mohammedk20259@gmail.com>
2025-10-02 11:05:39 -0400

    changes

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,36 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CellPhone myPhone = new CellPhone();
 
+        System.out.print("What is the serial number?: ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        myPhone.setSerialNumber(serialNumber);
+
+        System.out.print("What is the model?: ");
+        String model = scanner.nextLine();
+
+        myPhone.setModel(model);
+
+        System.out.print("What is the carrier?: ");
+        String carrier = scanner.nextLine();
+
+        myPhone.setModel(carrier);
+
+        System.out.print("What is the phone number?: ");
+        String phoneNumber = scanner.nextLine();
+
+        myPhone.setModel(phoneNumber);
+
+        System.out.print("What is the owner?: ");
+        String owner = scanner.nextLine();
+
+        myPhone.setModel(owner);
     }
 }
